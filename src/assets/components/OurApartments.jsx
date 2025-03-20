25d4ccd5e94593b868011122fd8a8ad0317284c0
sanathsb98 <itssanathsb@gmail.com>
2025-03-20 17:40:52 +0530

    commit

--- src/assets/components/OurApartments.jsx
+++ src/assets/components/OurApartments.jsx
@@ -7,7 +7,7 @@ const OurApartments = () => {
         <div className='our-apartments-section'>
             <div className='our-apartments-content'>
                 <div className='our-apartments-header'>
-                    <div className='our-aprtments-title'>Our Biophilic Apartment Projects in Kochi</div>
+                    <div className='our-apartments-title'>Our Biophilic Apartment Projects in Kochi</div>
                     <div className='our-apartments-des'>Get to know Kerala’s Most Trusted Builder - Veegaland Homes’ Biophilic Luxury Apartments in Kochi</div>
                     <div className='projects-toggle-sec'>
                         <div className='proj-toggle-btn proj-btn-active'>All Projects</div>

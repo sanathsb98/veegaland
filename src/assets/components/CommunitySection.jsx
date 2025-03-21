1783cfe18dd9da36f0eab02c1ce0d88704fa9958
sanathsb98 <itssanathsb@gmail.com>
2025-03-21 10:44:24 +0530

    commit

---
+++ src/assets/components/CommunitySection.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../components/CommunitySection.css';
+import communityimg from '../Images/communitybg.png';
+
+const CommunitySection = () => {
+  return (
+    <div className='community-section'>
+
+  
+    <div className='community-header'>A Community That Feels Like Family</div>
+      <div className='community-description'>Hear from those who have experienced the comfort and joy of living at Veegaland Biophilic Urban Homes</div>
+      <div className='community-videos-section'>kk</div>
+      <div className='community-btns'>
+        <div className='community-watch-btn'>Watch Videos</div>
+        <div className='community-learn-btn'>Learn More</div>
+      </div>
+
+    </div>
+  )
+}
+
+export default CommunitySection
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../pages/Home.css';
 import Banner from '../components/Banner';
 import OurApartments from '../components/OurApartments';
+import CommunitySection from '../components/CommunitySection';
 
 const Home = () => {
   return (
     <div className='home-page'>
       <Banner/>
       <OurApartments/>
+      <CommunitySection/>
     </div>
   )
 }

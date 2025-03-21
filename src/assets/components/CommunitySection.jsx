7e7ed05637172f8e5082bd8e2f933a38073e3b1a
sanathsb98 <itssanathsb@gmail.com>
2025-03-21 12:06:25 +0530

    commit

--- src/assets/components/CommunitySection.jsx
+++ src/assets/components/CommunitySection.jsx
@@ -8,6 +8,7 @@ import slide1 from '../Images/slide1.png';
 import slide2 from '../Images/slide2.png';
 import slide3 from '../Images/slide3.png';
 import slide4 from '../Images/slide4.png';
+import slide5 from '../Images/slide4.png';
 
 const CommunitySection = () => {
   const [mounted, setMounted] = useState(false);
@@ -28,28 +29,38 @@ const CommunitySection = () => {
       </div>
 
       <div className='community-videos-section'>
-        <Swiper
-          modules={[Autoplay]}
-          spaceBetween={360}
-          slidesPerView={4}
-          autoplay={{
-            delay: 0, // No delay between slides
-            disableOnInteraction: false,
-            pauseOnMouseEnter: false,
-            reverseDirection: false, // Set to `true` for reverse scroll
-          }}
-          speed={4000} // Adjusts scroll speed (lower = faster)
-          loop={true} // Enables infinite looping
-          freeMode={true} // Allows smooth sliding instead of snapping
-          allowTouchMove={false} // Disables user dragging
-          grabCursor={false} // Removes grab cursor effect
-        >
-          {slides.map((img, index) => (
-            <SwiperSlide key={index}>
-              <img src={img} alt={`Slide ${index}`} className="w-full h-auto rounded-lg" />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      <Swiper
+  modules={[Autoplay]}
+
+  autoplay={{
+    delay: 0,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: false,
+    reverseDirection: false,
+  }}
+  speed={5000} // Smooth scrolling speed
+  loop={true} 
+  freeMode={true} 
+  allowTouchMove={false} 
+  grabCursor={false} 
+  breakpoints={{
+    100: { slidesPerView: 3, spaceBetween: 200 }, // Below 1000px → Show 3 slides
+    300: { slidesPerView: 1, spaceBetween: 200 }, // 300px - 480px → Show 1 slide
+    480: { slidesPerView: 2, spaceBetween: 500 }, // 480px - 750px → Show 2 slides
+    750: { slidesPerView: 2, spaceBetween: 200 }, // 750px - 950px → Show 3 slides
+    950: { slidesPerView: 3, spaceBetween: 200 }, // Over 950px → Show 4 slides
+    1750: { slidesPerView: 4, spaceBetween: 340 }, // Over 950px → Show 4 slides
+ 
+  }}
+>
+  {slides.map((img, index) => (
+    <SwiperSlide key={index}>
+      <img src={img} alt={`Slide ${index}`} className="w-full h-auto rounded-lg" />
+    </SwiperSlide>
+  ))}
+</Swiper>
+
+
       </div>
 
       <div className='community-btns'>
